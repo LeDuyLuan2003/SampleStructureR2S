d2b6444919be8d3c92199ddd1b81f08acba94f59
DuyLuan <luanml0016@gmail.com>
2025-04-23 20:51:13 +0700

    23/04/2025: Create mapper, exception, ApiResponse, constant

--- src/main/java/com/r2s/ApiWebReview/mapper/UserMapper.java
+++ src/main/java/com/r2s/ApiWebReview/mapper/UserMapper.java
@@ -2,26 +2,17 @@ package com.r2s.ApiWebReview.mapper;
 
 import com.r2s.ApiWebReview.dto.UserResponse;
 import com.r2s.ApiWebReview.entity.User;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserResponse toResponse(User user);
-
-    default UserResponse toResponseWithRole(User user) {
-        UserResponse dto = toResponse(user);
-        if (user.getRole() != null) {
-            dto.setRole(user.getRole().getName());
-        } else {
-            dto.setRole(null);
-        }
-        return dto;
-    }
-//        @Mapping(target = "role", source = "role.name")
-//        UserResponse toResponseWithRole(User user);
+    @Mapping(target = "role", source = "role.name")
+    UserResponse toResponseWithRole(User user);
 
 }
 
